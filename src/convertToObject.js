e5dca0b06b317d364e87b463e4c4790926d041f0
MorelSami <mksami237@gmail.com>
2025-03-03 21:50:51 -0600

    testing

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,12 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const obj = {};
+  sourceString.split(';').forEach((style) => {
+    const separatorIndex = style.indexOf(':');
+    obj[style.slice(0, separatorIndex)] = style.slice(separatorIndex + 1);
+  });
+  return obj;
 }
 
 module.exports = convertToObject;
